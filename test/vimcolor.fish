99268f8d7a6d476874a7a50840c6675d83753ac3
Ryota Kobayashi <ryotakov@gmail.com>
2017-05-06 00:34:24 +0900

    Add tests for the default colorscheme

--- test/vimcolor.fish
+++ test/vimcolor.fish
@@ -1,3 +1,67 @@
-test "description"
-	-n "expression"
+set -l old_fish_color_end $fish_color_end
+set -l old_fish_color_match $fish_color_match
+set -l old_fish_color_escape $fish_color_escape
+set -l old_fish_color_selection $fish_color_selection
+
+vimcolor default >/dev/null ^/dev/null
+
+test "fish_color_end"
+    "$old_fish_color_end" = "$fish_color_end"
 end
+
+test "fish_color_match"
+    "$old_fish_color_match" = "$fish_color_match"
+end
+
+test "fish_color_escape"
+    "$old_fish_color_escape" = "$fish_color_escape"
+end
+
+test "fish_color_selection"
+    "$old_fish_color_selection" = "$fish_color_selection"
+end
+
+test "fish_color_command"
+    "ffff60 --bold" = "$fish_color_command"
+end
+
+test "fish_color_quote"
+    "ffa0a0" = "$fish_color_quote"
+end
+
+test "fish_color_redirection"
+    "Cyan" = "$fish_color_redirection"
+end
+
+test "fish_color_error"
+    "White --background=Red" = "$fish_color_error"
+end
+
+test "fish_color_param"
+    "40ffff" = "$fish_color_param"
+end
+
+test "fish_color_comment"
+    "80a0ff" = "$fish_color_comment"
+end
+
+test "fish_color_search_match"
+    "Black --background=Yellow" = "$fish_color_search_match"
+end
+
+test "fish_color_operator"
+    "ffff60 --bold" = "$fish_color_operator"
+end
+
+test "fish_color_autosuggestion"
+    "80a0ff" = "$fish_color_autosuggestion"
+end
+
+test "fish_color_valid_path"
+    "80a0ff --underline" = "$fish_color_valid_path"
+end
+
+test "fish_color_history_current"
+    "Cyan" = "$fish_color_history_current"
+end
+

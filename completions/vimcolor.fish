5b413405ba892ddc64732762024d8fbb7fd6cdfd
Ryota Kobayashi <ryotakov@gmail.com>
2017-05-05 22:16:40 +0900

    Add a completion

--- completions/vimcolor.fish
+++ completions/vimcolor.fish
@@ -1,1 +1,17 @@
-complete -c vimcolor -d 'convert a vim-colorschem into a fish-colorscheme!'
+function __vimcolor_colorschemes
+    vim -es -u '~/.vimrc' \
+    +'set nonumber' \
+    +'redir @a' \
+    +'echo globpath(&runtimepath, \'colors/*.vim\')' \
+    +'redir END' \
+    +'put a' \
+    +'%p' \
+    +'q!' | while read -l line
+        set -l scheme (string match -r '([^/]+)\.vim' $line)
+        and echo $scheme[2]
+    end
+    true
+end
+
+complete -c vimcolor -d 'convert a vim-colorscheme into a fish-colorscheme!'
+complete -f -c vimcolor -a '(__vimcolor_colorschemes)'
